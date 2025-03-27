5a90f00eb5b4f816ec6442daf96420667993a2a3
Truongjava <thanhtruong16042004@gmail.com>
2025-03-27 15:39:45 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,32 +1,11 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   // base: "./", 
-//   plugins: [react()],
-//   server: {
-//     port: 5173,
-//     host: true,
-//     proxy: {
-//       "/api": {
-//         target: "https://visual-caption-backend.onrender.com",
-//         changeOrigin: true,
-//         secure: false,
-//         rewrite: (path) => path.replace(/^\/api/, "/api"), 
-//       },
-//     },    
-//   },
-//   build: {
-//     outDir: "dist", 
-//   },
-// });
-
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // base: "./", 
   plugins: [react()],
   server: {
-    port: process.env.PORT || 0, // Cho phép chọn cổng ngẫu nhiên
+    port: 5173,
     host: true,
     proxy: {
       "/api": {
@@ -35,9 +14,12 @@ export default defineConfig({
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "/api"), 
       },
-    },
+    },    
   },
   build: {
-    outDir: "dist",
+    outDir: "dist", 
   },
 });
+
+
+
