241a25464f8334de54d25457f8b38d033fea6e7b
TruongLe <128674613+Truongjava@users.noreply.github.com>
2025-04-23 10:27:07 +0700

    Update gemini_helper.py

--- backend/app/utils/gemini_helper.py
+++ backend/app/utils/gemini_helper.py
@@ -4,7 +4,8 @@ async def generate_caption(image_bytes: bytes) -> str:
     model = genai.GenerativeModel("gemini-1.5-flash-001")
 
     # Prompt yêu cầu sinh caption bằng tiếng Việt
-    prompt = "Hãy mô tả bức ảnh này bằng tiếng Việt một cách chi tiết."
+    # prompt = "Hãy mô tả bức ảnh này bằng tiếng Việt một cách chi tiết."
+    prompt = "Hãy mô tả bức ảnh này bằng tiếng Việt chỉ bằng 1 câu. chỉ mô tả đặc điểm chính. mô tả ngắn gọn."
 
     response = model.generate_content([
         prompt,
